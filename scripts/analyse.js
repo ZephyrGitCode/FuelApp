f587befdf2985899f804980061d9caed455751df
ZephyrGitCode <zephyr.dobson@outlook.com>
2019-09-25 15:14:29 +0930

    optimze analyse script

--- scripts/analyse.js
+++ scripts/analyse.js
@@ -5,15 +5,23 @@ var d = new Date();
 var n = d.getMonth();
 document.getElementById("cm").innerHTML = monthNames[n];
 
-//Calculate total of cost
+//for each local storage (already there)
+//for each month if(current local storage data is within the speceific month)
+//create a new div with totals of local storage(only if div doesn't exist)
+//bunch of variables
+
 var costtotal = 0;
 var litrestotal = 0;
 for(var i = 1; i < localStorage.length+1; i++) {
   var data = JSON.parse(localStorage.getItem(i));
 
-  if(data != null && data["total"] != "" && data["fuel"] != ""){
-    costtotal+=parseInt(data["total"]);
-    litrestotal+=parseInt(data["fuel"]);
+  if(data != null){
+    if(data["total"] != ""){
+      costtotal+=parseInt(data["total"]);
+    }
+    if(data["fuel"] != ""){
+      litrestotal+=parseInt(data["fuel"]);
+    }
 
     console.log(new Date(data["date"]).getMonth());
   }
@@ -22,6 +30,7 @@ for(var i = 1; i < localStorage.length+1; i++) {
 /*
 for(var ii=0; ii < monthNames.length; ii++){
   var currentmonth = new Date(data["date"]).getMonth();
+  if currentmonth === ii
 
 };
 */
