d16cb9d4c5c4574e2f2da09f0b514e5011f05e24
ZephyrGitCode <zephyr.dobson@outlook.com>
2019-10-11 20:21:21 +0930

    Changes total depending on month

--- scripts/analyse.js
+++ scripts/analyse.js
@@ -10,22 +10,56 @@ document.getElementById("cm").innerHTML = monthNames[n];
 //create a new div with totals of local storage(only if div doesn't exist)
 //bunch of variables
 
-var costtotal = 0;
-var litrestotal = 0;
-for(var i = 1; i < localStorage.length+1; i++) {
-  var data = JSON.parse(localStorage.getItem(i));
 
-  if(data != null){
-    if(data["total"] != ""){
-      costtotal+=parseInt(data["total"]);
-    }
-    if(data["fuel"] != ""){
-      litrestotal+=parseInt(data["fuel"]);
-    }
 
-    console.log(new Date(data["date"]).getMonth());
-  }
+choosedate()
+
+function choosedate(evt) {
+  var costtotal = 0;
+  var litrestotal = 0;
+
+  var select = document.getElementById('datedrop').value;
+
+  for(var i = 1; i < localStorage.length+1; i++) {
+    var data = JSON.parse(localStorage.getItem(i));
+
+    if(data != null && (new Date(data["date"]).getMonth()) == select){
+      if(data["total"] != ""){
+        costtotal+=parseInt(data["total"]);
+      }
+      if(data["fuel"] != ""){
+        litrestotal+=parseInt(data["fuel"]);
+      }
+    }
+  };
+
+  document.getElementById("c").innerHTML = "$"+costtotal;
+  document.getElementById("t").innerHTML = litrestotal+"L";
 };
+
+document.getElementById('datedrop').addEventListener('click', choosedate);
+
+  /*
+  newtable = document.createElement('table');
+  newtable.setAttribute("id", ".logtable");
+
+  var cost = document.createElement('td');
+  var litres = document.createElement('td');
+
+  cost.innerHTML = "$"+costtotal;
+  litres.innerHTML = litrestotal+"L";
+
+  var row = document.createElement('tr');
+  row.appendChild(cost);
+  row.appendChild(litres);
+  document
+    .querySelector('.logtable')
+    .appendChild(row);
+};
+
+
+
+
 
 /*
 for(var ii=0; ii < monthNames.length; ii++){
@@ -34,16 +68,3 @@ for(var ii=0; ii < monthNames.length; ii++){
 
 };
 */
-
-var cost = document.createElement('td');
-var litres = document.createElement('td');
-
-cost.innerHTML = "$"+costtotal;
-litres.innerHTML = litrestotal+"L";
-
-var row = document.createElement('tr');
-row.appendChild(cost);
-row.appendChild(litres);
-document
-  .querySelector('.logtable')
-  .appendChild(row);
