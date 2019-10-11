800fb7bef099a0966644aa5a9435fd9980ed691f
ZephyrGitCode <zephyr.dobson@outlook.com>
2019-10-12 00:33:39 +0930

    Fixed analyse bug

--- scripts/analyse.js
+++ scripts/analyse.js
@@ -3,47 +3,43 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December"];
 var d = new Date();
 var n = d.getMonth();
-document.getElementById("cm").innerHTML = monthNames[n];
 
-//for each local storage (already there)
-//for each month if(current local storage data is within the speceific month)
-//create a new div with totals of local storage(only if div doesn't exist)
-//bunch of variables
+document.getElementById('datedrop').value = n;
 
-var costtotal = 0;
-var litrestotal = 0;
-for(var i = 1; i < localStorage.length+1; i++) {
-  var data = JSON.parse(localStorage.getItem(i));
+choosedate()
 
-  if(data != null){
-    if(data["total"] != ""){
-      costtotal+=parseInt(data["total"]);
-    }
-    if(data["fuel"] != ""){
-      litrestotal+=parseInt(data["fuel"]);
-    }
+function choosedate(evt) {
+  var costtotal = 0;
+  var litrestotal = 0;
+  const odomath = [];
 
-    console.log(new Date(data["date"]).getMonth());
-  }
+  var select = document.getElementById('datedrop').value;
+
+  for(var i = 1; i < localStorage.length+1; i++) {
+    var data = JSON.parse(localStorage.getItem(i));
+
+    if(data != null && (new Date(data["date"]).getMonth()) == select){
+      if(data["total"] != ""){
+        costtotal+=parseInt(data["total"]);
+      };
+      if(data["fuel"] != ""){
+        litrestotal+=parseInt(data["fuel"]);
+      };
+      if(data["odo"] != ""){
+        odomath.push(parseInt(data["odo"]));
+      };
+    };
+  };
+
+  document.getElementById("c").innerHTML = "$"+costtotal;
+  document.getElementById("t").innerHTML = litrestotal+"L";
+  document.getElementById("o").innerHTML = 0+"km";
+
+  console.log(odomath);
+  if(odomath.length != 0){
+    var maxodo = Math.max(...odomath);
+    document.getElementById("o").innerHTML = maxodo+"km";
+  };
 };
 
-/*
-for(var ii=0; ii < monthNames.length; ii++){
-  var currentmonth = new Date(data["date"]).getMonth();
-  if currentmonth === ii
-
-};
-*/
-
-var cost = document.createElement('td');
-var litres = document.createElement('td');
-
-cost.innerHTML = "$"+costtotal;
-litres.innerHTML = litrestotal+"L";
-
-var row = document.createElement('tr');
-row.appendChild(cost);
-row.appendChild(litres);
-document
-  .querySelector('.logtable')
-  .appendChild(row);
+document.getElementById('datedrop').addEventListener('click', choosedate);
