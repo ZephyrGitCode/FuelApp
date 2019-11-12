22b0dc649351316162aa085663635518ba98d276
ZephyrGitCode <zephyr.dobson@outlook.com>
2019-11-12 14:20:39 +0930

    cors

--- scripts/localapi.js
+++ scripts/localapi.js
@@ -4,16 +4,26 @@
 const url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=shops&key=AIzaSyCYtSvriljMuTDh3Xc2ZkCBR4WHaJ5ohtI"
 
 const otherparams = {
-  method: 'post',
+  method: 'get',
   mode: 'cors',
+  credentials: 'include',
+  origin: "https://zephyrgitcode.github.io/FuelApp/",
   headers:{
-    "content=type":"application/json;"
-    }
-  }
+    "Content-Type":"application/json;",
+    "Access-Control-Allow-Origin":"https://zephyrgitcode.github.io/FuelApp/"
+  },
+  body:undefined
+};
 
 fetch(url,otherparams).then(data=>{return data.json()}).then(res=>{console.log(res)});
 
 /*
+
+"Authorization":"AIzaSyCYtSvriljMuTDh3Xc2ZkCBR4WHaJ5ohtI;",
+'Access-Control-Allow-Origin': "https://zephyrgitcode.github.io/FuelApp/",
+'Access-Control-Allow-Credentials': true,
+'Access-Control-Allow-Methods':'POST, GET'
+
 https://maps.googleapis.com/maps/api/place/nearbysearch/json
   ?location=-33.8670522,151.1957362
   &radius=500
